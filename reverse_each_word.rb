92c1d3f05df4cd345a2fcf8492fa1b4eecbf5e66
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 02:26:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,14 +4,14 @@
 
 # then use .each on every new element in the new arrray so you can use .reverse on every element and puts it out to the console.
 
-# def reverse_each_word(string)
-#   reversed = []
-#   array = string.split(" ")
-#   array.each do |word|
-#     reversed << word.reverse
-#   end
-#   reversed.join(" ")
-# end
+def reverse_each_word(string)
+  reversed = []
+  array = string.split(" ")
+  array.each do |word|
+    reversed << word.reverse
+  end
+  reversed.join(" ")
+end
 
 def reverse_each_word(string)
   array = string.split(" ")
