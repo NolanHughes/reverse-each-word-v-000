ee283f68aa225b2c0463b2b43b34feb4c3d5dc44
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 02:09:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
-def reverse_each_word(string)
-  string.reverse
-end
+# def reverse_each_word(string)
+#   string.reverse
+# end
 
 #split each word in the string with a method to make it an array - .split? - and then use .each on every new element in the new arrray so you can use .reverse on every element and puts it out to the console.
 
 def reverse_each_word(string)
-  
+  string.split(" , ")
+end
